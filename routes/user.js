c5fb72716a53492f3d1594fe53cde3a476ea9114
suhag_sarak <suhag.sarak@phenompeople.com>
2021-12-18 13:50:16 +0530

    login working

--- routes/user.js
+++ routes/user.js
@@ -27,5 +27,16 @@ router.post('/', (request, response) => {
     })
 })
 
+router.post('/isValid', (request, response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+    const connection = db.connect()
+    const statement = `select pid, name, email, gender, address, city, password from user where email="${email}" and password="${password}";`
+    connection.query(statement, (error, data) => {
+        connection.end()
+        response.send(data)
+    })
+})
+
 
 module.exports = router
