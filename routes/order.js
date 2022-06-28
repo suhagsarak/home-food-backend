da65b830bb3a35e7ddec5a5197f1a1784f8105ed
suhag_sarak <suhag.sarak@phenompeople.com>
2022-06-29 00:16:28 +0530

    apis done

--- routes/order.js
+++ routes/order.js
@@ -4,111 +4,6 @@ const db = require("../db");
 
 const router = express.Router();
 
-// show all orders
-router.get("/", (request, response) => {
-  const statement = "select * from orders";
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    connection.end();
-    response.send(data);
-  });
-});
-
-// show all owner-orders
-router.get("/owner-orders", (request, response) => {
-  const statement = "select oid, time, totalPrice, uid, status from orders";
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    connection.end();
-    response.send(data);
-  });
-});
-
-// show all owner-orders with user join
-router.get("/owner-orders-with-user", (request, response) => {
-  const statement = "select O.oid, O.time, O.totalPrice, O.status, U.uid, U.name,  U.email, U.gender, U.address,  U.city, U.type from orders O INNER JOIN user U where O.uid = U.uid";
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    connection.end();
-    response.send(data);
-  });
-});
-
-// show all owner-orders with user join
-router.get("/order-details", (request, response) => {
-  const oid = request.query.oid;
-  const statement = `select O.oid, O.time, O.totalPrice, O.status, U.uid, U.name,  U.email, U.gender, U.address,  U.city, U.type from orders O INNER JOIN user U where O.uid = U.uid and O.oid="${oid}"`;
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    if (data[0]) {
-      const statement1 = `select P.pid, P.name, P.category, P.price, PO.count from orders O INNER JOIN product P INNER JOIN  productsinorders PO  where PO.oid = O.oid and PO.pid = P.pid and O.oid = "${oid}"`;
-      connection.query(statement1, (error1, data1) => {
-        data[0].products = data1;
-        connection.end();
-        response.send(data[0]);
-      });
-    }
-  });
-});
-
-// get user specific orders
-router.get("/userOrder", (request, response) => {
-  const uid = request.query.uid;
-  const statement = `select * from orders where uid=${uid}`;
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    connection.end();
-    response.send(data);
-  });
-});
-
-// get single order details with product list
-router.get("/details", (request, response) => {
-  const oid = request.query.oid;
-  const statement1 = `select * from orders where oid=${oid}`;
-  const connection = db.connect();
-  connection.query(statement1, (error, data) => {
-    if (!error && data && data.length) {
-      const order = data[0];
-      const statement2 = `select P.pid, P.name, P.category, P.price, OP.count from Product P INNER JOIN productsinorders OP on OP.pid = P.pid where OP.oid = ${oid}`;
-      connection.query(statement2, (error, data) => {
-        if (!error) {
-          connection.end();
-          order.products = data;
-          response.send(order);
-        } else {
-          connection.end();
-          response.send(data);
-        }
-      });
-    } else {
-      connection.end();
-      response.send(data);
-    }
-  });
-});
-
-// cancel order update
-router.get("/cancel", (request, response) => {
-  const oid = request.query.oid;
-  const statement = `update orders set status="CANCELLED" where oid=${oid}`;
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    connection.end();
-    response.send(data);
-  });
-});
-
-// delete orders
-router.get("/delete", (request, response) => {
-  const oid = request.query.oid;
-  const statement = `delete from orders where oid=${oid}`;
-  const connection = db.connect();
-  connection.query(statement, (error, data) => {
-    connection.end();
-    response.send(data);
-  });
-});
 
 // add orders
 router.post("/create", (request, response) => {
@@ -141,5 +36,137 @@ router.post("/create", (request, response) => {
     }
   });
 });
+
+// show all orders
+router.get("/", (request, response) => {
+  const statement = "select * from orders";
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+// show all owner-orders with user join
+router.get("/owner-orders-with-user", (request, response) => {
+  const statement = "select O.oid, O.time, O.totalPrice, O.status, U.uid, U.name,  U.email, U.gender, U.address,  U.city, U.type from orders O INNER JOIN user U where O.uid = U.uid";
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+// show all customer-orders with user join
+router.get("/customer-orders", (request, response) => {
+  const uid = request.query.uid;
+  const statement = `select O.oid, O.time, O.totalPrice, O.status, U.uid, U.name,  U.email, U.gender, U.address,  U.city, U.type from orders O INNER JOIN user U where O.uid = U.uid && O.uid = ${uid}`;
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+// show all owner-orders with user join
+router.get("/order-details", (request, response) => {
+  const oid = request.query.oid;
+  const statement = `select O.oid, O.time, O.totalPrice, O.status, U.uid, U.name,  U.email, U.gender, U.address,  U.city, U.type from orders O INNER JOIN user U where O.uid = U.uid and O.oid="${oid}"`;
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    if (data[0]) {
+      const statement1 = `select P.pid, P.name, P.category, P.price, PO.count from orders O INNER JOIN product P INNER JOIN  productsinorders PO  where PO.oid = O.oid and PO.pid = P.pid and O.oid = "${oid}"`;
+      connection.query(statement1, (error1, data1) => {
+        data[0].products = data1;
+        connection.end();
+        response.send(data[0]);
+      });
+    }
+  });
+});
+
+// assign-delivery-person update
+router.get("/assign-delivery-person", (request, response) => {
+  const oid = request.query.oid;
+  const dpid = request.query.dpid;
+  const statement = `update orders set dpid=${dpid} where oid=${oid}`;
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+// update order passed status
+router.get("/update-status", (request, response) => {
+  const oid = request.query.oid;
+  const status = request.query.status;
+  const statement = `update orders set status="${status}" where oid=${oid}`;
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+// cancel order update
+router.get("/cancel", (request, response) => {
+  const oid = request.query.oid;
+  const statement = `update orders set status="CANCELLED" where oid=${oid}`;
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+// delete orders
+router.get("/delete", (request, response) => {
+  const oid = request.query.oid;
+  const statement = `delete from orders where oid=${oid}`;
+  const connection = db.connect();
+  connection.query(statement, (error, data) => {
+    connection.end();
+    response.send(data);
+  });
+});
+
+
+// // get user specific orders
+// router.get("/userOrder", (request, response) => {
+//   const uid = request.query.uid;
+//   const statement = `select * from orders where uid=${uid}`;
+//   const connection = db.connect();
+//   connection.query(statement, (error, data) => {
+//     connection.end();
+//     response.send(data);
+//   });
+// });
+
+// // get single order details with product list
+// router.get("/details", (request, response) => {
+//   const oid = request.query.oid;
+//   const statement1 = `select * from orders where oid=${oid}`;
+//   const connection = db.connect();
+//   connection.query(statement1, (error, data) => {
+//     if (!error && data && data.length) {
+//       const order = data[0];
+//       const statement2 = `select P.pid, P.name, P.category, P.price, OP.count from Product P INNER JOIN productsinorders OP on OP.pid = P.pid where OP.oid = ${oid}`;
+//       connection.query(statement2, (error, data) => {
+//         if (!error) {
+//           connection.end();
+//           order.products = data;
+//           response.send(order);
+//         } else {
+//           connection.end();
+//           response.send(data);
+//         }
+//       });
+//     } else {
+//       connection.end();
+//       response.send(data);
+//     }
+//   });
+// });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -12,6 +12,15 @@ router.get('/', (request, response) => {
     })
 })
 
+router.get('/deliverypersons', (request, response) => {
+    const connection = db.connect()
+    const statement = `select dpid, name, email, gender, address, city, type, password from deliverypersons;`
+    connection.query(statement, (error, data) => {
+        connection.end()
+        response.send(data)
+    })
+})
+
 router.post('/', (request, response) => {
     const name = request.body.name;
     const email = request.body.email;
@@ -21,7 +30,14 @@ router.post('/', (request, response) => {
     const type = request.body.type;
     const password = request.body.password;
     const connection = db.connect()
-    const statement = `insert into user (name, email, gender, address, city, type, password) values ("${name}", "${email}", "${gender}", "${address}", "${city}", "${type}", "${password}");`
+
+    let statement;
+    if (type == 'Delivery Person') {
+        statement = `insert into deliveryperson(name, email, gender, address, city, type, password) values ("${name}", "${email}", "${gender}", "${address}", "${city}", "${type}", "${password}");`
+    } else {
+        statement = `insert into user (name, email, gender, address, city, type, password) values ("${name}", "${email}", "${gender}", "${address}", "${city}", "${type}", "${password}");`
+    }
+
     connection.query(statement, (error, data) => {
         connection.end()
         response.send(data)
@@ -33,7 +49,13 @@ router.post('/isValid', (request, response) => {
     const password = request.body.password;
     const type = request.body.type;
     const connection = db.connect()
-    const statement = `select uid, name, email, gender, address, city, type, password from user where email="${email}" and password="${password}" and type="${type}";`
+
+    let statement;
+    if (type == 'Delivery Person') {
+        statement = `select dpid, name, email, gender, address, city, type, password from deliveryperson where email="${email}" and password="${password}" and type="${type}";`
+    } else {
+        statement = `select uid, name, email, gender, address, city, type, password from user where email="${email}" and password="${password}" and type="${type}";`
+    }
     connection.query(statement, (error, data) => {
         connection.end()
         response.send(data)
